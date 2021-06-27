e0ed9d2d7ecda78785b2d1e29230dcc47e1673b7
Felipe Fernandes <felipe@govegans.it>
2021-06-27 20:03:41 +0200

    cors

--- routes/routes.js
+++ routes/routes.js
@@ -4,12 +4,6 @@ const express = require('express')
 var router = express.Router()
 
 router.get('/stops', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Max-Age", "1000000000");
-
     console.log("Getting data")
      controller.getRoutesEAV("ETR001")
      .then((data) => {
