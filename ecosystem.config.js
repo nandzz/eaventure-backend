ab0f0f639534f3c113058efdf72238560dae9b68
Felipe Fernandes <felipe@govegans.it>
2021-06-27 15:05:25 +0200

    changing extension

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps : [
+        {
+          name: "app",
+          script: "./bin/www",
+          instances: 1,
+          exec_mode: "cluster",
+          watch: true,
+          env: {
+              "PORT": 3000,
+              "NODE_ENV": "development"
+          }
+        }
+    ]
+  }
+  
