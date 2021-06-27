97667e0d56590f31c9e4a8a2722d0ac19ad5b999
Felipe Fernandes <felipe@govegans.it>
2021-06-27 17:47:02 +0200

    adding exclusion

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
           autorestart: true,
           max_memory_restart: '1G',
           watch: true,
+          ignore_watch: [".git/index.lock", "logs", "public", "node_modules", "[\\/\\\\]\\./","pids", ".git", ".idea"],
           env: {
               "PORT": 3000,
               "NODE_ENV": "production"
