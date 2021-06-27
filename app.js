15538ae8f8c21569476bbc1087b5dc595ae07a03
Felipe Fernandes <felipe@govegans.it>
2021-06-27 12:06:08 +0200

    express 4

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const express = require('express')
-const http = require('http')
-const path = require('path')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const config = require('./config')
-const debug = require('debug')('app4')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const { Router } = require('express');
-const cors = require('cors')
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-var   app = express()
-      env = app.settings.env
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-const routes = require('./routes.js')
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors())
-
-app.use('/', routes)
-
-http.createServer(app).listen(3000, function(){
-    console.log("Listening")
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
