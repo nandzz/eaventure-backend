2d25f09861232733b0b88844d07e408848cef886
Felipe Fernandes <felipe@govegans.it>
2021-06-27 14:51:14 +0200

    removed env

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,12 +4,7 @@ module.exports = {
           name: "app",
           script: "./bin/www",
           instances: 1,
-          exec_mode: "cluster",
           watch: true,
-          env: {
-              "PORT": 3000,
-              "NODE_ENV": "development"
-          }
         }
     ]
   }
